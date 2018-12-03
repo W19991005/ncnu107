ea4b71591a696f3224b90fcdc825592010f7e8d7
W19991005 <45555209+W19991005@users.noreply.github.com>
2018-12-03 16:34:21 +0800

    Add files via upload

---
+++ 20181119_ex01/20181119_ex01.ino
@@ -0,0 +1,35 @@
+#define blueLed 13
+#define redLed 12
+
+void setup() {
+  // put your setup code here, to run once:
+    pinMode(blueLed,OUTPUT) ;
+    pinMode(redLed,OUTPUT) ;
+    digitalWrite(blueLed,LOW) ;
+    digitalWrite(redLed,LOW) ;
+    
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    digitalWrite(blueLed,HIGH) ;
+    digitalWrite(redLed,HIGH) ;
+    delay(500) ;
+    digitalWrite(blueLed,LOW) ;
+    digitalWrite(redLed,LOW) ;
+    delay(500) ;
+    digitalWrite(blueLed,HIGH) ;
+    digitalWrite(redLed,HIGH) ;
+    delay(500) ;
+    digitalWrite(blueLed,LOW) ;
+    digitalWrite(redLed,LOW) ;
+    delay(1000) ;
+    digitalWrite(redLed,HIGH) ;
+    delay(1000) ;
+    digitalWrite(redLed,LOW) ;
+    delay(1000) ;
+    digitalWrite(blueLed,HIGH) ;
+    delay(1000) ;
+    digitalWrite(blueLed,LOW) ;
+    delay(1000) ;
+}
---
+++ 20181126_ex02/20181126_ex02.ino
@@ -0,0 +1,22 @@
+#define analog0 0
+#define led1 9
+#define led2 10
+#define led3 11
+
+int analogvalue = 0 ;
+
+void setup() {
+  // put your setup code here, to run once:
+    Serial.begin(9600) ;
+    Serial.println("Program Start") ;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogvalue = analogRead(analog0) ;
+  Serial.println(analogvalue) ;
+  analogWrite(led1,analogvalue/4) ;
+  analogWrite(led2,analogvalue/4) ;
+  analogWrite(led3,analogvalue/4) ;
+  delay(200) ;
+}
